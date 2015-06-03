dcd24f3506733cac9e99c250ba81a82d4c627dc6
Xinran Xiao <xinranxiao@gmail.com>
2015-06-03 01:37:34 -0700

    server work

---
+++ spotify_server.js
@@ -0,0 +1,13 @@
+Accounts.addAutopublishFields({
+  forLoggedInUser: _.map(
+    // publish access token since it can be used from the client
+    // refresh token probably shouldn't be sent down.
+    Spotify.whitelistedFields.concat(['accessToken', 'expiresAt']), // don't publish refresh token
+    function (subfield) { return 'services.spotify.' + subfield; }),
+
+  forOtherUsers: _.map(
+    // even with autopublish, no legitimate web app should be
+    // publishing all users' emails
+    _.without(Spotify.whitelistedFields, 'email'),
+    function (subfield) { return 'services.spotify.' + subfield; })
+});
